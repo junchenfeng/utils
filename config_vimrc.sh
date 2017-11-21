c23bb58e2d0e434dd9b43f569cd09390a40db522
Junchen Feng <frankfeng.pku@gmail.com>
2017-11-21 14:38:57 +0800

    增加vimrc调控和vim8的安装

---
+++ config_vimrc.sh
@@ -0,0 +1,21 @@
+### VIM Basic
+set -e
+# install color scheme
+mkdir -p ~/.vim/colors && cd ~/.vim/colors
+wget -O wombat256mod.vim http://www.vim.org/scripts/download_script.php?src_id=13400
+
+# folding
+mkdir -p ~/.vim/ftplugin
+wget -O ~/.vim/ftplugin/python_editing.vim http://www.vim.org/scripts/download_script.php?src_id=5492
+
+
+# intall packages with vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+cp ~/.vimrc ~/.vimrc_bak
+cp .vimrc ~/.vimrc
+
+vim +PluginInstall +qall
+# need to properly install relevent Go libraries
+vim :GoInstallBinaries
+
